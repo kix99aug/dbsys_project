039985e77667ee0ffff3e5e4fbd6636875776407
Hanyu "Kix" Huang <kix99aug@gmail.com>
2019-12-26 22:09:28 +0800

    debug

--- mylinebot/views.py
+++ mylinebot/views.py
@@ -53,6 +53,7 @@ def callback(request):
                             data = user_stage[event.source.user_id]['data']['result'][idx]['data']
                             columns = []
                             for i in data:
+                                print(data)
                                 columns.append(CarouselColumn(thumbnail_image_url=data[i]['image_url'], title=data[i]['title'], text='$'+str(
                                     data[i]['price']), actions=[URITemplateAction(label='前往購買', uri=data[i]['url'])]))
                             line_bot_api.push_message(
